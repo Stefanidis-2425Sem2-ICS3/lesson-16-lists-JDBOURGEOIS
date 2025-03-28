f4234bf20a33a702968802067aed4646b3f2b0f7
JDBOURGEOIS <bourgeoj@students.dsbn.org>
2025-03-28 12:23:53 -0400

    done

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+#Julian.Bourgeois
+#march 24th 2025
+import random
+numbers = []
+for i in range (100):
+    num = random.randint (0,100)
+    numbers.append(num)
 
+avg = sum(numbers) /len (numbers)
+print(numbers)
+print(avg)
+
+...
